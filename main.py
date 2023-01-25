772c338cd0a64fac278ac81a8d7e58c62cf7bcd5
Phill <PhilippDup@gmai.com>
2023-01-25 17:17:33 +0200

    Ädded main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Imports
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+
+# Class OOP
+class GUI(tkinter.Tk):
+    def __int__(self):
+        super().__int__()
+
+
+# TK GUI
+root = GUI()
+root.mainloop()
