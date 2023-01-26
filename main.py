d385b12efb469bec079e48df522a94acc0474204
Phill <PhilippDup@gmai.com>
2023-01-26 16:00:14 +0200

    Basic GUI complete

--- main.py
+++ main.py
@@ -15,25 +15,41 @@ class AppGui(tk.Tk):
         # Frame
         # self.frame = tk.Frame(self, bg='blue')
 
+    def create_num_buttons(self):
+
+        row = 1
+        col = 0
+        for btn_num in range(0, 10):
+
+            if btn_num == 0:
+                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num))
+                btn.grid(column=1, row=4)
+            else:
+
+                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num))
+                btn.grid(column=col, row=row)
+
+                if btn_num % 3 == 0:
+                    col = 0
+                    row += 1
+                else:
+                    col += 1
+
     def create_widgets(self):
         # Insert --> Equation
         edt_main = tk.Entry(self, width=28, bd=3, font=('Arial', 24))
         edt_main.grid(column=0, row=0, columnspan=3, sticky=tk.N)
 
-        row = 1
-        col = 0
-        for btn_num in range(1, 10):
-            btn = tk.Button(self, width=5, bd=3, font=('Arial', 24), text=str(btn_num))
-            btn.grid(column=col, row=row)
-
-            if btn_num % 3 == 0:
-                col = 0
-                row += 1
-            else:
-                col += 1
+        btn_plus = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str('+'))
+        btn_plus.grid(column=0, row=5)
+        btn_minus = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str('-'))
+        btn_minus.grid(column=1, row=5)
+        btn_mult = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str('X'))
+        btn_mult.grid(column=2, row=5)
 
 
 if __name__ == "__main__":
     Gui = AppGui()
+    Gui.create_num_buttons()
     Gui.create_widgets()
     Gui.mainloop()
