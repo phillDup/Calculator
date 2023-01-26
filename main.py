ee8ede1f79af40b19fec13dd6e266ffc21bd87f2
Phill <PhilippDup@gmai.com>
2023-01-26 15:34:29 +0200

    Added input bar and btn 1-9

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 # Imports
-import tkinter
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 
-window = tkinter.Tk()
-window.geometry('500x500')
 
-frm = tkinter.Frame(window, width=100, height=100)
+# Main GUI OOP
+class AppGui(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-window.mainloop()
+        # Main Window
+        self.geometry('513x500')
+        self.title('Calculator')
+
+        # Frame
+        # self.frame = tk.Frame(self, bg='blue')
+
+    def create_widgets(self):
+        # Insert --> Equation
+        edt_main = tk.Entry(self, width=28, bd=3, font=('Arial', 24))
+        edt_main.grid(column=0, row=0, columnspan=3, sticky=tk.N)
+
+        row = 1
+        col = 0
+        for btn_num in range(1, 10):
+            btn = tk.Button(self, width=5, bd=3, font=('Arial', 24), text=str(btn_num))
+            btn.grid(column=col, row=row)
+
+            if btn_num % 3 == 0:
+                col = 0
+                row += 1
+            else:
+                col += 1
+
+
+if __name__ == "__main__":
+    Gui = AppGui()
+    Gui.create_widgets()
+    Gui.mainloop()
