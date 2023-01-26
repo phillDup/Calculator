44a589cd957c7931aa16bd4c18481fd7555a57e7
Phill <PhilippDup@gmai.com>
2023-01-26 16:19:42 +0200

    clsEquation added

---
+++ clsEquation.py
@@ -0,0 +1,7 @@
+class Equation:
+    def __init__(self):
+        self.equation = []
+
+    def add_num(self, num):
+        self.equation.append(num)
+        print(self.equation)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import tkinter as tk
 from tkinter import *
 
+import clsEquation
+
 
 # Main GUI OOP
 class AppGui(tk.Tk):
@@ -12,9 +14,10 @@ class AppGui(tk.Tk):
         self.geometry('513x500')
         self.title('Calculator')
 
-        # Frame
-        # self.frame = tk.Frame(self, bg='blue')
+        # Equation Object
+        self.equation = clsEquation.Equation()
 
+    # Function automatically creates number buttons
     def create_num_buttons(self):
 
         row = 1
@@ -22,11 +25,11 @@ class AppGui(tk.Tk):
         for btn_num in range(0, 10):
 
             if btn_num == 0:
-                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num))
+                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num), command= lambda : self.equation.add_num(0))
                 btn.grid(column=1, row=4)
             else:
 
-                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num))
+                btn = tk.Button(self, width=8, bd=3, font=('Arial', 24), text=str(btn_num),  command= lambda : self.equation.add_num(3))
                 btn.grid(column=col, row=row)
 
                 if btn_num % 3 == 0:
@@ -35,6 +38,7 @@ class AppGui(tk.Tk):
                 else:
                     col += 1
 
+    # Function to add other widgets
     def create_widgets(self):
         # Insert --> Equation
         edt_main = tk.Entry(self, width=28, bd=3, font=('Arial', 24))
@@ -52,4 +56,5 @@ if __name__ == "__main__":
     Gui = AppGui()
     Gui.create_num_buttons()
     Gui.create_widgets()
+
     Gui.mainloop()
